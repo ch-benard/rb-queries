a7067fc05acc54a062ce3fbd9ac9628dfe185b7e
Christophe BENARD <ch.benard@gmail.com>
2021-10-27 11:51:54 +0200

    (bdd-234) Summary of activities updated Updates made following feedback from the business development team: - City added - Department added - name or address added - Simplified water access indicator - Simplified electricity access indicator - Simplified trash collection indicator - Simplified toilet access indicator

--- 2021-10-26_recap_activites.sql
+++ 2021-10-26_recap_activites.sql
@@ -1,9 +1,42 @@
 SELECT
+	c.name AS "Commune",
+	d.name AS "Département",
+	s.shantytown_id AS "Identifiant",
+	CASE 
+	WHEN s."name" IS NOT NULL THEN s."name"
+	ELSE s."address"
+	END AS "Nom du site",
 	TO_CHAR(s.updated_at, 'DD/MM/YYYY hh:mm:ss') AS "Date de dernière mise à jour",
 	comments."Nombre de commentaires",
 	"Nombre d'acteurs",
 	"Nombre de dispositifs",
 	-- Conditions d'accès à l'eau
+	CASE access_to_water
+	WHEN TRUE
+	THEN
+		CASE
+		WHEN 
+			s.water_potable IS NULL OR s.water_potable = FALSE OR
+			s.water_continuous_access IS NULL OR s.water_continuous_access = FALSE OR
+            s.water_public_point IS NULL OR s.water_public_point = FALSE OR
+            s.water_distance IS NULL OR s.water_distance != '0-20' OR
+            s.water_roads_to_cross IS NULL OR s.water_roads_to_cross = TRUE OR
+            s.water_everyone_has_access IS NULL OR s.water_everyone_has_access = FALSE OR
+            s.water_stagnant_water IS NULL OR s.water_stagnant_water = TRUE OR
+            s.water_hand_wash_access IS NULL OR s.water_hand_wash_access = FALSE OR
+            COALESCE(s.population_total, 0) / COALESCE(s.water_hand_wash_access_number, 1) < 20
+        THEN
+        	'A améliorer'
+        ELSE
+        	'Oui'
+        END
+    WHEN FALSE
+    THEN
+    	'Non'
+	ELSE
+		'Inconnu'
+	END
+	AS "Accès à l'eau",
 	CONCAT(
 	--access_to_water
 	CASE  access_to_water
@@ -23,7 +56,7 @@ SELECT
 	ELSE NULL
 	END,
 	-- water_public_point
-		CASE water_public_point
+	CASE water_public_point
 	WHEN TRUE THEN CONCAT('Point d''accès public', CHR(13),CHR(10))
 	ELSE NULL
 	END,
@@ -73,7 +106,153 @@ SELECT
 	ELSE
 		NULL
 	END
-	) AS "Conditions d''accès à l''eau"
+	) AS "Conditions d'accès à l'eau",
+	-- Accès aux toilettes
+	CASE access_to_sanitary
+	WHEN TRUE
+	THEN
+		CASE
+		WHEN 
+			s.sanitary_insalubrious IS NULL OR s.sanitary_insalubrious = TRUE OR
+			s.sanitary_on_site IS NULL OR s.sanitary_on_site = FALSE OR
+            COALESCE(s.population_total, 0) / COALESCE(s.sanitary_number, 1) < 20
+        THEN
+        	'A améliorer'
+        ELSE
+        	'Oui'
+        END
+    WHEN FALSE
+    THEN
+    	'Non'
+	ELSE
+		'Inconnu'
+	END
+	AS "Accès aux toilettes",
+	-- Conditions d'accès aux toilettes
+	CONCAT(
+	-- toilettes insalubres
+	CASE sanitary_insalubrious
+	WHEN TRUE THEN CONCAT('Marques de défécation à l''air libre', CHR(13),CHR(10))
+	WHEN FALSE THEN CONCAT('Pas de marques de défécation à l''air libre  ', CHR(13),CHR(10))
+	ELSE NULL
+	END,
+	-- sanitary_on_site
+	CASE sanitary_on_site
+	WHEN TRUE THEN CONCAT('Accès aux toilettes sur site', CHR(13),CHR(10))
+	WHEN FALSE THEN CONCAT('Pas d''accès aux toilettes sur site', CHR(13),CHR(10))
+	ELSE NULL
+	END,
+	-- sanitory_access_ratio
+	CASE
+	WHEN population_total > 0
+	THEN
+		CASE access_to_sanitary
+		WHEN TRUE 
+		THEN 
+			CASE 
+			WHEN sanitary_number > 0
+			THEN
+				CASE 
+				WHEN sanitary_number > population_total 
+				THEN CONCAT('Plus d''1 toilette par personne',  CHR(13),CHR(10))
+				ELSE
+					CONCAT('1 toilette pour ', population_total / sanitary_number, CHR(13),CHR(10))
+				END
+			ELSE NULL
+			END
+		ELSE
+			NULL
+		END
+	ELSE
+		NULL
+	END
+	) AS "Conditions d'accès aux toilettes",
+	-- Evacuation des déchets
+	CASE trash_evacuation
+	WHEN TRUE
+	THEN
+		CASE
+		WHEN 
+			s.trash_cans_on_site IS NULL OR s.trash_cans_on_site < 1 OR
+			s.trash_evacuation_regular IS NULL OR s.trash_evacuation_regular = FALSE OR
+			s.trash_accumulation IS NULL OR s.trash_accumulation = TRUE
+        THEN
+        	'A améliorer'
+        ELSE
+        	'Oui'
+        END
+    WHEN FALSE
+    THEN
+    	'Non'
+	ELSE
+		'Inconnu'
+	END
+	AS "Evacuation des déchets",
+	-- Conditions d'évacuations des déchets
+	CONCAT(
+	-- trash_cans_on_site
+	CASE
+	WHEN trash_cans_on_site > 0
+	THEN CONCAT(trash_cans_on_site, ' poubelle(s) / benne(s) à proximité immédiate du site (moins de 100 mètres)', CHR(13),CHR(10))
+	ELSE NULL
+	END,
+	-- trash_acumulation
+	CASE trash_accumulation
+	WHEN TRUE
+	THEN CONCAT('Accumulation de déchets sur le site ou aux abords', CHR(13),CHR(10))
+	WHEN FALSE
+	THEN CONCAT('Pas d''accumulation de déchets sur le site ou aux abords', CHR(13),CHR(10))
+	ELSE NULL
+	END,
+	-- trash_evacuation_regular
+	CASE trash_evacuation_regular
+	WHEN TRUE
+	THEN CONCAT('Collecte régulière des poubelles / bennes.', CHR(13),CHR(10))
+	WHEN FALSE
+	THEN CONCAT('Pas de collecte régulière des poubelles / bennes.', CHR(13),CHR(10))
+	ELSE NULL
+	END
+	) AS "Conditions d'évacuations des déchets",
+	-- Accès à l'électricité
+	CONCAT(
+	CASE fk_electricity_type
+	WHEN 3 THEN CONCAT('Oui', CHR(13), CHR(10))
+	WHEN 2 THEN CONCAT('Non', CHR(13), CHR(10))
+	WHEN 1 THEN CONCAT('Inconnu', CHR(13), CHR(10))
+	ELSE NULL
+	END
+	) AS "Accès à l'électricité",
+	-- Présence de nuisible
+	CONCAT(
+	CASE vermin
+	WHEN TRUE THEN CONCAT('Présence de nuisibles sur site ou à proximité', CHR(13), CHR(10))
+	WHEN FALSE THEN CONCAT('Pas de nuisibles sur site ou à proximité', CHR(13), CHR(10))
+	ELSE NULL
+	END,
+	CASE
+	WHEN vermin_comments IS NOT NULL THEN CONCAT(vermin_comments, CHR(13), CHR(10))
+	ELSE NULL
+	END
+	) AS "Présence de nuisibles",
+	-- Prévention incendie
+	CASE fire_prevention_measures
+	WHEN TRUE THEN CONCAT('Oui', CHR(13), CHR(10))
+	WHEN FALSE THEN CONCAT('Non', CHR(13), CHR(10))
+	ELSE 'Inconnu'
+	END AS "Prévention incendie",
+	-- Détail prévention incendie
+	CASE 
+	WHEN fire_prevention_comments IS NOT NULL
+	THEN fire_prevention_comments
+	ELSE NULL
+	END
+	AS "Mesures de prévention incendie",
+	-- Objectif résorption
+	CASE s.resorption_target
+	WHEN '2021'
+	THEN 'oui'
+	END
+	AS "Objectif résorption 2021"
 FROM
 	shantytowns s
 LEFT JOIN
@@ -113,5 +292,11 @@ LEFT JOIN
 	GROUP BY
 		ps.fk_shantytown
 	) AS dispositifs ON dispositifs.shantytown_id = s.shantytown_id
-WHERE
-	s.resorption_target = '2021'
+LEFT JOIN
+	-- Ville (cities)
+	cities c ON c.code =s.fk_city
+LEFT JOIN
+	-- Département
+	departements d ON d.code = c.fk_departement
+ORDER BY
+	d.name, c.name, s.shantytown_id
